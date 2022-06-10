36858a422d39fb21ac8602f8e9911d4493d6bc22
Luis Carlos <lucaldev22@gmail.com>
2022-06-10 02:01:21 -0300

    Changes on header and about section.

--- web/src/container/Header/Header.jsx
+++ web/src/container/Header/Header.jsx
@@ -8,17 +8,17 @@ export default function Header() {
     <header className='app__header'>
       <nav className='app__header-nav'>
         <ul className='app__header-nav-content'>
-          <div className='app__header-nav-div'>
-            <li>
-              <a href='#Explanation'>Como jogar?</a>
+          <div className='app__header-nav-div app__header-nav-div-1'>
+          <li>
+              <a href='#About'>Sobre</a>
             </li>
             <li>
-              <a href='#About'>Sobre</a>
+              <a href='#Explanation'>Como jogar?</a>
             </li>
           </div>
           <div>
             <li>
-              <a href='#'><h1>NAÇÕES</h1></a>
+              <h1><a href='#'>NAÇÕES</a></h1>
             </li>
           </div>
           <div className='app__header-nav-div'>
